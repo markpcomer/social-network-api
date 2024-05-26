7183f759a4dcb2be956e488bdc9539909172c67d
markpcomer <markpcomer@gmail.com>
2024-05-26 19:25:24 -0400

    coded thought.js

---
+++ models/thought.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const reaction = require('./reaction');
+
+const thoughtSchema = new Schema(
+    {
+       thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1, 
+            maxLength: 280,
+       }, 
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => {
+                return new Date(timestamp).toISOString();
+            }
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reaction: [reaction]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+      }
+);
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function() {
+        return this.reaction.length;
+    });
+
+    const Thought = model('thought', thoughtSchema);
+
+    module.exports = Thought;
--- models/user.js
+++ models/user.js
@@ -34,15 +34,16 @@ const userSchema = new Schema(
             },
         ],
     },
-        {
+    {
         toJSON: {
             virtuals: true,
         },
             id: false,
-        }  
+    }  
 );
 
-userSchema.virtual('friendCount')
+userSchema
+    .virtual('friendCount')
     .get(function () {
         return `${this.friends}`;
     });
