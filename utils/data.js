d7fa92585c11c7af1dd38f896f7fd5179a12e806
markpcomer <markpcomer@gmail.com>
2024-05-27 15:08:25 -0400

    added utils/data.js. about to code seed.js

---
+++ utils/data.js
@@ -0,0 +1,47 @@
+const userSeedData = [
+    {
+        username: 'ernststavro',
+        email: 'blofeld@spectre.org'
+    },
+    {
+        username: 'auricgoldfiner',
+        email: 'ireallylikegold@spectre.org'
+    },
+    {
+        username: 'alectrevelyan',
+        email: '006@mi6.co.uk'
+    },
+    {
+        username: 'oddjob',
+        email: 'haberdasker@spectre.org'
+    },
+    {
+        username: 'lechiffre',
+        email: 'customerservice@bankofamerica.com'
+    },
+];
+
+const thoughtSeedData = [
+    {
+        thought: 'I Shall Look Forward To Personally Exterminating You, Mr. Bond.',
+        username: 'ernststavro',
+    },
+    {
+        thought: 'No Mr. Bond, I expect you to die.',
+        username: 'auricgoldfiner',
+    },
+    {
+        thought: 'You know, James... I was always better.',
+        username: 'alectrevelyan',
+    },
+    {
+        thought: '...',
+        username: 'haberdasker@spectre.org'
+    },
+    {
+        thought: 'I hope our little game isn\'t causing you to perspire.',
+        username: 'lechiffre',
+    }
+];
+
+module.exports = { userSeedData, thoughtSeedData };
