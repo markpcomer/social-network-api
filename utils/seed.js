a4727858198c0a546ceee5605d72396dc437c7f1
markpcomer <markpcomer@gmail.com>
2024-05-27 15:17:47 -0400

    coded seed.js

--- utils/seed.js
+++ utils/seed.js
@@ -1,8 +1,20 @@
 const connection = require('/config/connection');
 const { User, Thought } = require('../models');
+const { userSeedData, thoughtSeedData } = require('./data');
 
 connection.on('error', (err) => err);
 
 connection.once('open', async() => {
     console.log('connected');
+
+    const user = userSeedData;
+    const thought = thoughtSeedData;
+
+    await User.collection.insertMany(user);
+    await Thought.collection.insertMany(thought);
+
+    console.table(user);
+    console.table(thought);
+    console.info('Seeding complete! 🌱');
+    process.exit(0);
 })
